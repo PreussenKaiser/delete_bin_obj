21b20d1ec0e4487261c73b76be7120bebe03dbb7
Karl Lukan <klukan20@gmail.com>
2022-02-06 21:45:53 -0600

    Working on delete functionality

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![allow(unused_must_use)]
 
+use std::io;
 use std::fs;
+use std::path::Path;
+use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
@@ -11,17 +14,20 @@ struct Cli {
 fn main() {
     let args = Cli::from_args();
 
+    let bin = String::from("bin");
+    let obj = String::from("obj");
+
     match args.command.as_str() {
         "help" => print_commands(),
 
         "delete" => {
-            delete_directories(String::from("bin"));
-            delete_directories(String::from("obj"));
+            delete_directories(&bin, &PathBuf::from("./"));
+            delete_directories(&obj, &PathBuf::from("./"));
         },
 
         "read" => {
-            read_directories(String::from("bin"));
-            read_directories(String::from("obj"));
+            read_directories(&bin);
+            read_directories(&obj);
         },
 
         _ => println!("{} is an unknown command!", args.command),
@@ -33,24 +39,34 @@ fn main() {
         println!("DELETE: Deletes bin and obj folders.");
         println!("READ: Lists all detected bin and obj folders.");
     }
+}
 
-    // Deletes bin and obj directories.
-    fn delete_directories(name: String) {
-        println!("Deleted {}", name);
-        fs::remove_dir_all(name);
+// Deletes bin and obj directories.
+fn delete_directories(name: &String, dir: &Path) -> io::Result<()> {      
+    for entry in fs::read_dir(&dir)? {
+        let entry = entry?;
+        let path = entry.path();
+
+        if path == PathBuf::from(&name) {
+            fs::remove_dir_all(path);
+        } else {
+            delete_directories(&name, &path);
+        }
     }
 
-    // Reades all detected bin and obj directories.
-    fn read_directories(name: String) {
-        let paths = fs::read_dir("./").unwrap();
+    Ok(())
+}
 
-        for path in paths {
-            let dir = format!("./{}", name);
-            let cur_dir = format!("{}", path.unwrap().path().display());
+// Reads all detected bin and obj directories.
+fn read_directories(name: &String) {
+    let paths = fs::read_dir("./").unwrap();
 
-            if cur_dir == dir {
-                println!("{}", cur_dir);
-            }
+    for path in paths {
+        let dir = format!("./{}", &name);
+        let cur_dir = format!("{}", &path.unwrap().path().display());
+
+        if &cur_dir == &dir {
+            println!("{}", &cur_dir);
         }
     }
 }
